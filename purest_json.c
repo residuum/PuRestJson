a442a50e3c55501f532b3ce177b8c2e0b50198e0
Hans-Christoph Steiner <hans@eds.org>
2012-01-28 23:25:35 -0500

    fixed setup function names in single-binary library file

--- purest_json.c
+++ purest_json.c
@@ -4,7 +4,7 @@ void purest_json_setup(void) {
 	post("PuREST JSON version %s: A library for executing HTTP queries and encoding and decoding JSON data from Puredata.", LIBRARY_VERSION);
 	post("(c) Thomas Mayer (Residuum) 2011");
 	post("Get the latest source from https://github.com/residuum/PuRestJson");
-	rest0x2djson_setup();
-	json0x2dencoder_setup();
-	json0x2ddecoder_setup();
+	setup_rest0x2djson();
+	setup_json0x2dencode();
+	setup_json0x2ddecode();
 }

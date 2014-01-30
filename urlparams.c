eb9bc4c41b6f214ef670f53da9a20c78ecfb5bc0
Thomas Mayer <thomas@residuum.org>
2014-01-30 23:50:10 +0100

    Make [urlparams] consistant with [json-encode]: Output empty symbol when empty.

--- urlparams.c
+++ urlparams.c
@@ -82,6 +82,7 @@ void urlparams_bang(t_urlparams *urlp) {
 	char *encoded_string = NULL;
 
 	if (urlp->storage.first_data == NULL) {
+		outlet_symbol(urlp->storage.x_ob.ob_outlet, gensym(""));
 		return;
 	}
 

3d8b793e93ad1d560a889bca263d9529f6b259be
Thomas Mayer <thomas@residuum.org>
2014-01-30 23:35:42 +0100

    Unit tests for [urlparams].

---
+++ unittests/urlparams-tests.py
@@ -0,0 +1,33 @@
+#! /usr/bin/python
+
+import unittest
+from pdStarter import runPd
+
+class UrlparamsTests(unittest.TestCase):
+    def test_add_single_value(self):
+        out = runPd('urlparams/urlparams-add-single-value.pd')
+        self.assertEquals(out, '''list key=value;
+''')
+
+    def test_add_two_values(self):
+        out = runPd('urlparams/urlparams-add-two-values.pd')
+        self.assertEquals(out, '''list key=value&key2=value2;
+''')
+
+    def test_replace_value(self):
+        out = runPd('urlparams/urlparams-replace-value.pd')
+        self.assertEquals(out, '''list key=other;
+''')
+
+    def test_clear(self):
+        out = runPd('urlparams/urlparams-clear.pd')
+        self.assertEquals(out, '''list ;
+''')
+
+    def test_escape_value(self):
+        out = runPd('urlparams/urlparams-escape-value.pd')
+        self.assertEquals(out, '''list key=value%20with%20%23spaces%26other%2bst%c3%bcff%20incl.%20%c3%9cml%c3%a4ute;
+''')
+
+if __name__ == '__main__': 
+    unittest.main()

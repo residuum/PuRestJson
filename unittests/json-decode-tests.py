b5519cd97346914d39c7792c5637f2475994a397
Thomas Mayer <thomas@residuum.org>
2014-02-05 00:34:30 +0100

    First tests for [json-decode].

---
+++ unittests/json-decode-tests.py
@@ -0,0 +1,23 @@
+#! /usr/bin/python
+
+import unittest
+from pdStarter import runPd
+
+class JsonDecodeTests(unittest.TestCase):
+    basePath = 'json-decode/'
+    def test_simple_object(self):
+        out = runPd(self.basePath + 'json-decode-simple-object.pd')
+        self.assertEquals(out, '''list key value;
+bang;
+''')
+
+    def test_multiple_members(self):
+        out = runPd(self.basePath + 'json-decode-multiple-members.pd')
+        self.assertEquals(out, '''list key value;
+list key2 2;
+list key3 0.1;
+bang;
+''')
+
+if __name__ == '__main__': 
+    unittest.main()

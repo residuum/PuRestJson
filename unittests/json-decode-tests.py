2b63d164dd8bccb5cbd71107640a8393510f83ff
Thomas Mayer <thomas@residuum.org>
2014-02-05 20:36:28 +0100

    Unit tests:
    - using os.path
    - removed temporary file

--- unittests/json-decode-tests.py
+++ unittests/json-decode-tests.py
@@ -1,18 +1,19 @@
 #! /usr/bin/python
 
 import unittest
+import os.path
 from pdStarter import runPd
 
 class JsonDecodeTests(unittest.TestCase):
-    basePath = 'json-decode/'
+    basePath = 'json-decode'
     def test_simple_object(self):
-        out = runPd(self.basePath + 'json-decode-simple-object.pd')
+        out = runPd(os.path.join(self.basePath, 'json-decode-simple-object.pd'))
         self.assertEquals(out, '''list key value;
 bang;
 ''')
 
     def test_multiple_members(self):
-        out = runPd(self.basePath + 'json-decode-multiple-members.pd')
+        out = runPd(os.path.join(self.basePath, 'json-decode-multiple-members.pd'))
         self.assertEquals(out, '''list key value;
 list key2 2;
 list key3 0.1;

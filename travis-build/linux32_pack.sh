3fb48ddd685340dcad2fbe5374838ac1c1a5bd05
Thomas Mayer <thomas@residuum.org>
2016-11-30 00:50:18 +0100

    Specify packages as 32 bit.

--- travis-build/linux32_pack.sh
+++ travis-build/linux32_pack.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-sudo chroot ${CHROOTDIR} bash -c "cd PuRestJson && make deken"
+sudo chroot ${CHROOTDIR} bash -c "cd PuRestJson && make machine=i386 deken"
 cp ${CHROOTDIR}/PuRestJson/*.tar.gz .

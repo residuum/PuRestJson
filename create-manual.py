dfb6129bd68ee9cfec3a0a4d7ac9184fb4d49699
Thomas Mayer <thomas@residuum.org>
2016-12-12 23:38:42 +0100

    Removed unnecessary text munging.

--- create-manual.py
+++ create-manual.py
@@ -1,37 +1,11 @@
-# converts the Wiki to generate the manual
+# Converts the project Wiki to html and adds it as manual.
 #
-# copied and adapted from https://gist.github.com/mrexmelle/659abc02ae1295d60647
+# Adapted from https://gist.github.com/mrexmelle/659abc02ae1295d60647
 
-import getopt
-import re, shutil, tempfile
 import os
 import subprocess
 import sys
 from bs4 import BeautifulSoup
-
-# http://stackoverflow.com/questions/4427542/how-to-do-sed-like-text-replace-with-python
-def sed_inplace(filename, pattern, repl):
-    '''
-    Perform the pure-Python equivalent of in-place `sed` substitution: e.g.,
-    `sed -i -e 's/'${pattern}'/'${repl}' "${filename}"`.
-    '''
-    # For efficiency, precompile the passed regular expression.
-    pattern_compiled = re.compile(pattern)
-
-    # For portability, NamedTemporaryFile() defaults to mode "w+b" (i.e., binary
-    # writing with updating). This is usually a good thing. In this case,
-    # however, binary writing imposes non-trivial encoding constraints trivially
-    # resolved by switching to text writing. Let's do that.
-    with tempfile.NamedTemporaryFile(mode = 'w', delete = False) as tmp_file:
-        with open(filename) as src_file:
-            for line in src_file:
-                tmp_file.write(pattern_compiled.sub(repl, line))
-
-    # Overwrite the original file with the munged temporary file in a
-    # manner preserving file attributes (e.g., permissions).
-    shutil.copystat(filename, tmp_file.name)
-    shutil.move(tmp_file.name, filename)
-
 
 wikiDir = '/tmp/PuRestJson.wiki/'
 exportDir = 'manual/'
@@ -51,8 +25,6 @@ for f in os.listdir(wikiDir):
         baseFile = os.path.splitext(os.path.basename(f))[0];
         htmlFile = baseFile + '.html'
         subprocess.call(['grip', wikiDir + f, '--export', '--no-inline', exportDir + htmlFile])
-        p = re.compile(exportDir)
-        sed_inplace(exportDir + htmlFile, p, '')
         # edit links to css, images and other pages
         htmlDoc = open(exportDir + htmlFile)
         soup = BeautifulSoup(htmlDoc, 'lxml')
@@ -64,7 +36,7 @@ for f in os.listdir(wikiDir):
         soup.head.append(css)
         for a in soup.findAll('a'):
             if a['href'].startswith('https://github.com/residuum/PuRestJson/wiki/'):
-                a['href'] = a['href'].replace('https://github.com/residuum/PuRestJson/wiki/', '')+'.html'
+                a['href'] = a['href'].replace('https://github.com/residuum/PuRestJson/wiki/', '') + '.html'
         for img in soup.findAll('img'):
             if img['src'].startswith('https://camo.githubusercontent.com'):
                 img['src'] = img['data-canonical-src'].replace('https://raw.github.com/residuum/PuRestJson/master/','')

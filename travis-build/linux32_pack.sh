f923ca2c8cc7babb3367bcd9121fedcc8719dcc4
Thomas Mayer <thomas@residuum.org>
2016-12-12 23:39:06 +0100

    Add missing trailing slash from Linux 32 bit path.

--- travis-build/linux32_pack.sh
+++ travis-build/linux32_pack.sh
@@ -2,7 +2,7 @@
 
 git clone https://github.com/residuum/PuRestJson.wiki.git /tmp/PuRestJson.wiki/
 
-python create-manual.py ${CHROOTDIR}/manual
+python create-manual.py ${CHROOTDIR}/manual/
 
 sudo chroot ${CHROOTDIR} bash -c "cd PuRestJson && make machine=i386 deken"
 cp ${CHROOTDIR}/PuRestJson/*.tar.gz .

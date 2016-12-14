97aa67c2603d95a8347d0b1506b69409b61d3205
Thomas Mayer <thomas@residuum.org>
2016-12-14 01:06:26 +0100

    Catch links and images.

--- create-manual.py
+++ create-manual.py
@@ -19,20 +19,22 @@ if len(sys.argv) > 2:
 print 'Input directory: ', wikiDir
 print 'Output directory: ', exportDir
 
-# rename all files containing '[' and ']' in names because Windows does not like those
+# rename all files containing '[' and ']' in names,
+# because Windows does not like those.
 for f in os.listdir(wikiDir):
-    cleaned = f.replace('[', '').replace(']', '')
+    cleaned = f.replace('[', '').replace(']', '').replace(':', '')
     if f != cleaned:
         os.rename(wikiDir + f, wikiDir + cleaned)
 
-# convert md files one-by-one
+# convert md files one-by-one.
 for f in os.listdir(wikiDir):
     if f.endswith('.md'):
         print 'Converting: ', f
         baseFile = os.path.splitext(os.path.basename(f))[0];
         htmlFile = baseFile + '.html'
-        subprocess.call(['grip', wikiDir + f, '--export', '--no-inline', exportDir + htmlFile])
-        # edit links to css, images and other pages
+        subprocess.call(['grip', wikiDir + f, '--export', '--no-inline', 
+            exportDir + htmlFile])
+        # edit links to css, images and other pages.
         htmlDoc = open(exportDir + htmlFile)
         soup = BeautifulSoup(htmlDoc, 'lxml')
         for s in soup.findAll('link'):
@@ -41,14 +43,22 @@ for f in os.listdir(wikiDir):
         css.attrs['rel'] = 'stylesheet'
         css.attrs['href'] = 'style.css'
         soup.head.append(css)
+        # internal links.
         for a in soup.findAll('a'):
             if a['href'].startswith('https://github.com/residuum/PuRestJson/wiki/'):
-                a['href'] = a['href'].replace('https://github.com/residuum/PuRestJson/wiki/', '')\
-                        .replace('[', '')\
-                        .replace(']', '') + '.html'
+                a['href'] = a['href']\
+                        .replace('https://github.com/residuum/PuRestJson/wiki/', '')\
+                        .replace('%5B', '')\
+                        .replace('%5D', '')\
+                        .replace(':', '') + '.html'
+        # images with link to itself.
         for img in soup.findAll('img'):
             if img['src'].startswith('https://camo.githubusercontent.com'):
-                img['src'] = img['data-canonical-src'].replace('https://raw.github.com/residuum/PuRestJson/master/','')
+                img['src'] = img['data-canonical-src']\
+                        .replace('https://raw.github.com/residuum/PuRestJson/master/manual/','')
+                del img['data-canonical-src']
+                a = img.parent
+                del a['href']
         # write changes back to file
         htmlDoc.close()
         html = soup.prettify('utf-8')

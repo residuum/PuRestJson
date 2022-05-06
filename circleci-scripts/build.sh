f55d1590a02f29bee72dd72247598d28b1ae4c97
Thomas Mayer <thomas@residuum.org>
2022-05-06 02:22:08 +0200

    Fix manual creation for Mac.

--- circleci-scripts/build.sh
+++ circleci-scripts/build.sh
@@ -4,7 +4,7 @@ set -e
 git submodule init
 git submodule update
 
-export PATH="$PATH:~/.local/bin"
+export PATH="$PATH:~/.local/bin:/Users/distiller/Library/Python/3.9/bin"
 
 bash ./circleci-scripts/${SYSTEM}/setup_environment.sh
 bash ./circleci-scripts/${SYSTEM}/compile.sh

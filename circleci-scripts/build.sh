bb73acda96e0df5f30a712351cda096b1cbf2b9c
Thomas Mayer <thomas@residuum.org>
2022-11-19 00:38:48 +0100

    Add timing to build script.

--- circleci-scripts/build.sh
+++ circleci-scripts/build.sh
@@ -6,7 +6,15 @@ git submodule update
 
 export PATH="$PATH:~/.local/bin:/Users/distiller/Library/Python/3.9/bin"
 
+SECONDS=0
 bash ./circleci-scripts/${SYSTEM}/setup_environment.sh
+echo "Time for setting up environment: ${SECONDS}s"
+SECONDS=0
 bash ./circleci-scripts/${SYSTEM}/compile.sh
+echo "Time for compilation: ${SECONDS}s"
+SECONDS=0
 bash ./circleci-scripts/${SYSTEM}/pack.sh
+echo "Time for packing: ${SECONDS}s"
+SECONDS=0
 bash ./circleci-scripts/upload.sh
+echo "Time for upload: ${SECONDS}s"

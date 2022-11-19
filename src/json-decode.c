residuum/PuRestJson

commit 96caf6ce83aff67d3301fb99ebaf542c53265085

Formatting of JSON string.
Implements #70

src/json-decode.c
@@ -80,13 +80,13 @@ static void jdec_output_object(json_object *const jobj, t_outlet *const data_out
 					SETFLOAT(&out_data[1], json_object_get_int(val));
 					break;
 				case json_type_string:
-					SETSYMBOL(&out_data[1], gensym(json_object_get_string(val)));
+					SETSYMBOL(&out_data[1], gensym(json_object_to_json_string_ext(val, JSON_C_TO_STRING_PLAIN)));
 					break;
 				case json_type_object:
-					SETSYMBOL(&out_data[1], gensym(json_object_get_string(val)));
+					SETSYMBOL(&out_data[1], gensym(json_object_to_json_string_ext(val, JSON_C_TO_STRING_PLAIN)));
 					break;
 				case json_type_array:
-					SETSYMBOL(&out_data[1], gensym(json_object_get_string(val)));
+					SETSYMBOL(&out_data[1], gensym(json_object_to_json_string_ext(val, JSON_C_TO_STRING_PLAIN)));
 					break;
 				case json_type_null:
 					SETSYMBOL(&out_data[1], gensym(""));

src/json-encode.c
@@ -119,15 +119,15 @@ static void jenc_load_json_object(const t_json_encode *const jenc, json_object *
 				break;
 			case json_type_string:
 				value = string_create(&value_len, snprintf(NULL, 0, "%s",
-							json_object_get_string(val)));
-				sprintf(value, "%s", json_object_get_string(val));
+							json_object_to_json_string_ext(val, JSON_C_TO_STRING_PLAIN)));
+				sprintf(value, "%s", json_object_to_json_string_ext(val, JSON_C_TO_STRING_PLAIN));
 				kvp_add_simple((struct _kvp_store *)jenc, key, kvp_val_create(value, 0));
 				string_free(value, &value_len);
 				break;
 			case json_type_object:
 				value = string_create(&value_len, snprintf(NULL, 0, "%s",
-							json_object_get_string(val)));
-				sprintf(value, "%s", json_object_get_string(val));
+							json_object_to_json_string_ext(val, JSON_C_TO_STRING_PLAIN)));
+				sprintf(value, "%s", json_object_to_json_string_ext(val, JSON_C_TO_STRING_PLAIN));
 				kvp_add_simple((struct _kvp_store *)jenc, key, kvp_val_create(value, 0));
 				string_free(value, &value_len);
 				break;
@@ -138,8 +138,8 @@ static void jenc_load_json_object(const t_json_encode *const jenc, json_object *
 					if (array_member != NULL) {
 						value = string_create(&value_len,
 								snprintf(NULL, 0, "%s",
-									json_object_get_string(array_member)));
-						sprintf(value, "%s", json_object_get_string(array_member));
+									json_object_to_json_string_ext(array_member, JSON_C_TO_STRING_PLAIN)));
+						sprintf(value, "%s", json_object_to_json_string_ext(array_member, JSON_C_TO_STRING_PLAIN));
 						kvp_add_array((struct _kvp_store *)jenc, key,
 								kvp_val_create(value, 0));
 						string_free(value, &value_len);
@@ -206,7 +206,7 @@ static t_symbol *jenc_get_json_symbol(t_json_encode *const jenc) {
 		}
 		json_object_object_add(jobj, it->key, value);
 	}
-	json_symbol = gensym(json_object_to_json_string(jobj));
+	json_symbol = gensym(json_object_to_json_string_ext(jobj, JSON_C_TO_STRING_PLAIN));
 	json_object_put(jobj);
 	return json_symbol;
 }

commit 3b63a19bf185bbd2cc4609c0e5edcddfd01e03c2

Strings are real strings.
Regression was introduced with commit 96caf6c

src/json-decode.c
@@ -79,7 +79,7 @@ static void jdec_output_object(json_object *const jobj, t_outlet *const data_out
 					SETFLOAT(&out_data[1], json_object_get_int(val));
 					break;
 				case json_type_string:
-					SETSYMBOL(&out_data[1], gensym(json_object_to_json_string_ext(val, JSON_C_TO_STRING_PLAIN)));
+					SETSYMBOL(&out_data[1], gensym(json_object_get_string(val)));
 					break;
 				case json_type_object:
 					SETSYMBOL(&out_data[1], gensym(json_object_to_json_string_ext(val, JSON_C_TO_STRING_PLAIN)));

src/json-encode.c
@@ -120,8 +120,8 @@ static void jenc_load_json_object(const t_json_encode *const jenc, json_object *
 				break;
 			case json_type_string:
 				value = string_create(&value_len, snprintf(NULL, 0, "%s",
-							json_object_to_json_string_ext(val, JSON_C_TO_STRING_PLAIN)));
-				sprintf(value, "%s", json_object_to_json_string_ext(val, JSON_C_TO_STRING_PLAIN));
+							json_object_get_string(val)));
+				sprintf(value, "%s", json_object_get_string(val));
 				kvp_add_simple((struct _kvp_store *)jenc, key, kvp_val_create(value, 0));
 				string_free(value, &value_len);
 				break;